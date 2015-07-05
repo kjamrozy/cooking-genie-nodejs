a5e7a65ff3e07ba1520890be63c70097fc62a883
Karol Jamrozy <karol.j.jamrozy@gmail.com>
2015-07-05 16:19:58 +0200

    Implement add restriction logic

--- app/routes/manage.js
+++ app/routes/manage.js
@@ -245,6 +245,52 @@ var diet_post_route = function(req,res,next){
 	});
 };
 
+/** Route used for adding new restrictions to the diets by using ajax requests.
+@param req - HTTP request
+@param res - server response
+@param next - callback for passing request to the next function
+ */
+var restriction_post_route = function(req,res,next){
+	//if there is no diet_id or product_id then it's not the call from /manage, so redirect to the /manage
+	if(!req.body.diet_id || !req.body.product_id)
+		return res.redirect('/manage');
+	/* days - stores days during which certain product is forbidden to eat, 
+	days are stored in binary form, for instance 74 which corresponds to 1001010
+	stands for days Tuesday,Thursday and Sunday*/
+	var days = 0;
+	//aux - auxiliary variable, used to calculate days value 
+	var aux = [req.body.monday,req.body.tuesday,req.body.wednesday,req.body.thursday,
+		req.body.friday,req.body.saturday,req.body.sunday];
+	//pow - initialized with 1 = 2^0
+	var pow = 1;
+	//in this loop we will compute days value by shifting aux and raising pow power by one for each iteration
+	while(aux.length>0){
+		//if product is forbidden on this days, add this power to the sum
+		if(aux[0])
+			days+=pow;
+		pow<<=1;
+		aux.shift();
+	}
+	//connect to the database to insert new restriction
+	pg.connect(conString,function(err,client,pg_done){
+		//if connection failed raise internal error
+	    if(err)
+	    	return raiseInternalError(err,client,pg_done,next);
+	    //insert restriction
+	    client.query("INSERT INTO Restriction(diet_id,product_id,days) VALUES ($1,$2,$3) ",
+	    	[req.body.diet_id,req.body.product_id,days],function(err,result){
+	    		//if query failed raise internal error
+				if(err)
+					return raiseInternalError(err,client,pg_done,next);
+
+				//release postgres client and redirect to the /manage page
+	    		pg_done();
+	    		req.flash('success',"Succesfully added restriction to the diet!");
+	    		res.redirect('/manage');
+    	});
+	});
+};
+
 router.get('/manage',manage_get_route);
 router.post('/products',products_post_route);
 router.post('/products/recipe',products_recipe_route);
@@ -254,5 +300,6 @@ router.get('/products/:id',function(req,res,next){
 	res.send("Not yet implemented!");
 });
 router.post('/diet',diet_post_route);
+router.post('/diet/restriction',restriction_post_route);
 
 module.exports = router;
